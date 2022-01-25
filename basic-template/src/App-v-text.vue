853f5d4577ccb5354b9a178a1439e678bf18777c
Thanyakorn <68809500+pisenuta@users.noreply.github.com>
2022-01-25 23:03:08 +0700

    ADD basic-template

---
+++ basic-template/src/App-v-text.vue
@@ -0,0 +1,15 @@
+<script setup>
+const msg = "Hello, Vus.js"
+const counter = 1
+</script>
+ 
+<template>
+  <h1> {{ msg.toLocaleLowerCase() }} </h1>
+  <!--<p v-text = "msg"></p>-->
+  <p> {{ (counter += 10) }} </p>
+</template>
+ 
+<style>
+
+</style>
+
---
+++ basic-template/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  const headingStyle = "color : blue"
+  const paraStyle = "text-decoration: underline"
+  const imagePath = "../public/images/mymelody.jfif" //server respose มา
+</script>
+ 
+<template>
+  <h1 v-bind:style = "headingStyle"> INT203 Web Client II</h1>
+  <p :style="paraStyle">lect. Umaporn</p>
+  <img src ="./assets/logo.png"> <!--รูปที่เอาไว้ตกแต่ง ไม่ต้องประมวลผลอะไร-->
+  <img :src = "imagePath" alt="" srcset=""> <!--รูปที่ต้อง bind ไว้ ต้องรอการประมวลผลก่อนว่าควรขึ้นรูปไหน-->
+</template>
+  
+<style>
+
+</style>
+
